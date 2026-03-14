b9caa0ff5fad25ae06dfd45a9a9d2fa0f786ef7b
Anuran Pradhan <anuranpradhan002@gmail.com>
2026-03-15 01:27:23 +0530

    min Shift Cards

---
+++ MinShiftCards.java
@@ -0,0 +1,40 @@
+package HWI_Preparation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class MinShiftCards {
+    public static int minShiftCards(List<String> cards, int s, String end) {
+        int n = cards.size();
+        int e = cards.indexOf(end);
+        if (s == e) {
+            return 0;
+        }
+        if (Math.abs(s - e) == 1) {
+            return 1;
+        }
+        int forward, backward;
+        if (e > s) {
+            forward = e - s;
+            backward = s + (n - 1 - e) + 1;
+        } else {
+            forward = e + (n - 1 - s) + 1;
+            backward = s - e;
+        }
+        return Math.min(forward, backward);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<String> cards = Arrays.asList(sc.nextLine().split(" "));
+        int s = sc.nextInt();
+        String target = sc.nextLine();
+
+        System.out.println(minShiftCards(cards, s, target));
+        sc.close();
+    }
+}
